962ac41c5cc3c527a3063e55e59b752f9e2b89a6
seanstory <skyloftproductions@Skylofts-iMac.hitronhub.home>
2022-01-17 17:34:48 -0800

    committing before deploy

--- sorted_project/settings.py
+++ sorted_project/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,8 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -139,4 +137,5 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = 'sorted_api.User'
 
-django_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
